a993bd7b4c3264856033b7da70c7e0bc66a6f9d8
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 20:07:25 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -29,6 +29,7 @@ read RESULT_COUNT < ${PIPE}
 echo ${RESULT_COUNT}
 query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=1"
 query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=2"
+query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=2"
 
 echo "Removing pipe:"
 rm -v ${PIPE}
