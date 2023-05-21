b137abcd279a3bfb88302bea288abc5ad37464ba
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 18:47:41 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env sh
 
 # Sample of the query:
+# https://docs.github.com/en/rest/search?apiVersion=2022-11-28#constructing-a-search-query
+
 curl -L   -H "Accept: application/vnd.github.text-match+json"   -H "Authorization: Bearer ****************************************"  -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/search/code?q=Q
