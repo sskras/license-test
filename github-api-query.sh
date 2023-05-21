76b8e0c75f57c274d006e28a94a2d5c352a263cb
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 19:28:06 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -4,7 +4,7 @@
 # https://docs.github.com/en/rest/search?apiVersion=2022-11-28#constructing-a-search-query
 
 GH_QUERY='"https%3A%2F%2Fblueoakcouncil.org%2Flicense%2F1.0.0."+per_page=100'
-TEXT_MATCH=".text-match"
+#TEXT_MATCH=".text-match"
 : ${GH_TOKEN:=`read -p "Enter the token (or just export via GH_TOKEN): "; echo $REPLY`}
 
 curl -L \
