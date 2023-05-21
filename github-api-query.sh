d77f79b05c5d8a354b00274b2dc8a41780a81c70
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 22:13:17 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -34,7 +34,7 @@ while
     cat ${BUFF}
     ITEM_COUNT=`{ jq '.items | length' | sed s/\\r//; } < ${BUFF}`
     RESULTS_COUNT=`{ jq '.total_count' | sed s/\\r//; } < ${BUFF}`
-    echo -e "Results count so far: ${RESULTS_COUNT}\n" >/dev/stderr
+    echo -e "\nResults count so far: ${RESULTS_COUNT}\n\n" >/dev/stderr
     sleep 2
 
     [ ${ITEM_COUNT} = ${GH_PER_PAGE} ]
