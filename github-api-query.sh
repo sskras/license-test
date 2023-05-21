d82809132aed8b170316eab4898479102f592f9b
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 22:09:59 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -3,13 +3,14 @@
 # Sample of the query:
 # https://docs.github.com/en/rest/search?apiVersion=2022-11-28#constructing-a-search-query
 
-PATH=$PATH:/mingw64/bin
-BUFF="`mktemp`.buffer"
-
 GH_QUERY='"https%3A%2F%2Fblueoakcouncil.org%2Flicense%2F1.0.0."'
 GH_PER_PAGE=100
 #TEXT_MATCH=".text-match"
 : ${GH_TOKEN:=`read -p "Enter the token (or just export via GH_TOKEN): "; echo $REPLY`}
+
+PATH=$PATH:/mingw64/bin
+BUFF="`mktemp`.buffer"
+
 
 query () {
     query="$1"
@@ -19,6 +20,7 @@ query () {
         -H "X-GitHub-Api-Version: 2022-11-28" \
         "https://api.github.com/search/code?q=${query}"
 }
+
 
 echo "Created buffer:"
 touch ${BUFF}
