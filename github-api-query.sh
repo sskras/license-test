77483a47ac14d51315e4ff47309a36b89dd30d28
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 21:22:41 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -51,7 +51,7 @@ query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=4" > ${BUFF}
     echo ${ITEM_COUNT}
     sleep 2
 
-    [ ${ITEM_COUNT} != 0 ]
+    [ ${ITEM_COUNT} = 0 ]
 do :; done
 
 #echo "Removing buffer:"
