ffb0131a3ebb1ab1afc3ce374ccda9a29851181e
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 21:19:05 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -34,22 +34,22 @@ query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=1" > ${BUFF}
 cat ${BUFF}
     read ITEM_COUNT < <(jq '.items | length' < ${BUFF})
     echo ${ITEM_COUNT}
-sleep 2
+    sleep 2
 query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=2" > ${BUFF}
 cat ${BUFF}
     read ITEM_COUNT < <(jq '.items | length' < ${BUFF})
     echo ${ITEM_COUNT}
-sleep 2
+    sleep 2
 query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=3" > ${BUFF}
 cat ${BUFF}
     read ITEM_COUNT < <(jq '.items | length' < ${BUFF})
     echo ${ITEM_COUNT}
-sleep 2
+    sleep 2
 query "${GH_QUERY}&per_page=${GH_PER_PAGE}&page=4" > ${BUFF}
 cat ${BUFF}
     read ITEM_COUNT < <(jq '.items | length' < ${BUFF})
     echo ${ITEM_COUNT}
-sleep 2
+    sleep 2
 cat ${BUFF} | jq .items
 
     [ $RESP != '[]' ]
