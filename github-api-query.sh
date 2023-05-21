64ebbc95d8d915577e4efffa02421150b22fc587
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-05-21 20:00:56 +0300

    "github-api-query.sh: Ordinary add."

--- github-api-query.sh
+++ github-api-query.sh
@@ -22,7 +22,8 @@ echo "Creating pipe:"
 mkfifo ${PIPE}
 ls -Alh --color ${PIPE}
 
-query "${GH_QUERY}&per_page=1" | jq .total_count
+query "${GH_QUERY}&per_page=1" | jq .total_count > ${PIPE}
+cat ${PIPE}
 query "${GH_QUERY}&per_page=100"
 
 echo "Removing pipe:"
